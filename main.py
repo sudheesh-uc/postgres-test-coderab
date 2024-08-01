5ddd9fbd18146d2664fbb06c56208b31aa07f5d2
sudheeshb <sudheeshb@unicourt.com>
2024-08-01 12:03:18 +0530

    check row is empty

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ try:
         column_names = [desc[0] for desc in cursor.description]
         writer.writerow(column_names)
 
-        if rows
-        # Write the data
-        writer.writerows(rows)
+        if rows:
+            # Write the data
+            writer.writerows(rows)
 
     print(f"Data has been written to {csv_file}")
 
